db071cfecd4762d831516d5f874ac1e550f56d2c
khushal7596 <khushalmalviya7596@gmail.com>
2025-11-22 18:47:58 +0530

    Add files via upload

---
+++ Sorting.java
@@ -0,0 +1,134 @@
+public class Sorting {
+    // public static void printArr(int arr[]){
+    //     for(int i = 0 ; i < arr.length; i++){
+    //         System.out.print(arr[i]+ " ");
+    //     }
+    // }
+    // public static void main(String [] args){
+
+        // bubble sort
+    //    for(int i = 0 ; i < arr.length-1 ; i++){
+    //     for(int j = 0 ; j <arr.length - i-1 ;j++){
+    //         if(arr[j]>arr[j+1]){
+    //             int temp = arr[j];
+    //             arr[j]=arr[j+1];
+    //             arr[j+1]= temp;
+    //         }
+
+    //     }
+    //    }
+    // printArr(arr);
+    
+    // selection sort 
+    //         int arr[] ={5 , 3 , 6 , 1, 9};
+    // for(int i = 0 ; i < arr.length-1; i++){
+        //     int small = i ;
+    //     for(int j = i+1 ;  j < arr.length-1;j++ )
+    //     if(arr[small]> arr[j]){
+        //         small = j;
+        //     }
+        //     int temp = arr[i];
+        //     arr[i] = arr[small];
+        //     arr[small] = temp;
+        // }
+        // printArr(arr);
+        // insertion sort
+        // int arr[] ={5 , 3 , 6 , 1, 9 , 78 , 43 , 4};
+        // for(int i = 1 ; i < arr.length;i++ ){
+        //     int current = arr[i];
+        //     int j = i-1;
+        //     while(j>=0 && current < arr[j]){
+        //         arr[j+1] = arr[j];
+        //         j--;
+        //     }
+        //     // current = arr[j+1];
+        //     arr[j+1] = current;
+
+        // }
+        // printArr(arr);
+
+
+        // merge sort 
+        // public static void divide(int arr [] , int si , int ei ){
+        //     if(si>= ei){
+        //         return ;
+        //     }
+        //     int mid = (si + ei)/2;
+        //     divide(arr, si , mid );
+        //     divide(arr, mid+1 , ei );
+        //     sort(arr , si , mid , ei);
+
+        // }
+        // public static void sort(int arr [] , int si , int mid , int ei ){
+        //    int merged [] = new int [ei - si +1];
+        //     int idx1 = si ;
+        //     int idx2 = mid+1;
+        //     int x = 0 ;
+        //     while(idx1 <= mid && idx2 <= ei){
+        //     if(arr[idx1] <= arr[idx2]){
+        //         merged[x++] = arr[idx1++];
+                
+                
+        //     }
+        //     else{
+        //         merged[x++] = arr[idx2++];
+        //     }}
+        //     while(idx1 <= mid){
+        //         merged[x++] = arr[idx1++];
+
+        //     }
+        //     while(idx2 <= ei){
+        //         merged[x++] = arr[idx2++];
+
+        //     }
+        //     for(int i= 0 , j = si ; i < merged.length ; i++ ,j++){
+        //         arr[j] = merged[i];
+        //     }
+        // }
+        // public static void main(String args []){
+        //     int arr [] = {2,6,9,3,5,8};
+        //     divide(arr , 0 , arr.length-1);
+        //     for(int i = 0 ; i < arr.length ; i++){
+        //         System.out.print(" "+arr[i]+" ");
+        //     }
+        // }
+        // Quick Sort 
+        public static int pivotIdx(int arr [] ,int  low , int high){
+            int pivot = arr[high];
+            int i = low -1 ;
+            for(int j = low ; j < high ; j++){
+                if(arr[j] < pivot){
+                    i++;
+                    int  temp = arr[i];
+                    arr[i] = arr[j] ;
+                    arr[j] = temp ;
+                }
+
+            }
+            i++ ;
+            int temp = arr[i];
+            arr[i] = pivot ;
+            arr[high] = temp ;
+            return i ;
+
+
+        }
+        public static void quickSort(int arr [] , int low , int high){
+            if(low < high){
+                int pivot = pivotIdx(arr , low , high);
+                quickSort(arr , low , pivot-1);
+                quickSort(arr , pivot+1 , high);
+            }
+        }
+        public static void main(String args []){
+            int arr [] = {2,6,9,3,8};
+            int n = arr.length;
+            quickSort(arr , 0 , n-1);
+            for(int i = 0 ; i < n ; i++){
+                System.out.print(arr[i]);
+            }
+        }
+        
+    }
+
+// }
